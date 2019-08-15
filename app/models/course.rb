5c0972f5704b566548f648a501a036d90eb7d5d9
Emilio Torres <emiliotorres75@gmail.com>
2019-08-15 15:32:46 +0000

    carrierwave

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,14 @@
 class Course < ApplicationRecord
+
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   has_many :sections
 
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+
+
+
 end
