5c0c9e62fd00867854d95d6d6b92a555268a9ccc
Emilio Torres <emiliotorres75@gmail.com>
2019-08-30 18:52:35 +0000

    footer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
       
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
   resource :dashboard, only: [:show]
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
